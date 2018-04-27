7dc8ff410048e5dcd12580ae638bc3a174cae262
Carlos Navarrete <cnavarreteliz@gmail.com>
2018-04-27 19:11:37 -0300

    fix mondrian_rest dependency

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup
 
 setup(
     name='datachile',
-    version='0.0.5',
+    version='0.0.6',
     description='Python client for manage DataChile API',
-    url='http://github.com/datawheel/datachile-api',
-    download_url='https://github.com/datawheel/datachile-api/archive/0.0.5.tar.gz',
+    url='http://github.com/datachile/datachile-python-client',
+    download_url='https://github.com/datachile/datachile-python-client/archive/0.0.6.tar.gz',
     author='Carlos Navarrete - Datawheel LLC',
     author_email='cnavarreteliz@gmail.com',
     license='MIT',
@@ -16,7 +16,7 @@ setup(
         'requests'
     ],
     dependency_links=[
-        "git+https://github.com/Datawheel/py-mondrian-rest.git#egg=mondrian_rest-0.1"
+        "git+https://github.com/datachile/datachile-python-client/tarball/9939446829ce42cbdefdf8c970a21deb9a5a4587#egg=mondrian_rest-0.1"
     ],
     keywords = ['python', 'mondrian', 'olap', 'api'],
     zip_safe=True
