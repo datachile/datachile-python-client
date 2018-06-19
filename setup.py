e5049b3a68846090160db91fbb9e8a719ca21aa6
Carlos Navarrete <cnavarreteliz@gmail.com>
2018-06-19 11:04:42 -0400

    minor correction in mondrian_rest library

--- setup.py
+++ setup.py
@@ -2,16 +2,16 @@ from setuptools import setup
 
 setup(
     name='datachile',
-    version='0.0.9',
+    version='0.0.10',
     description='Python client for manage DataChile API',
     url='http://github.com/datachile/datachile-python-client',
-    download_url='https://github.com/datachile/datachile-python-client/archive/0.0.9.tar.gz',
+    download_url='https://github.com/datachile/datachile-python-client/archive/0.0.10.tar.gz',
     author='Carlos Navarrete - Datawheel LLC',
     author_email='cnavarreteliz@gmail.com',
     license='MIT',
     packages=['datachile'],
     install_requires=[
-        'mondrian-rest',
+        'mondrian_rest',
         'numpy',
         'requests'
     ],
