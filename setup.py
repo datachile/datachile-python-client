0e4d7cab216ae89746af516ac8a5e3f0edd89be2
Carlos Navarrete <cnavarreteliz@gmail.com>
2018-06-19 11:16:01 -0400

    update mondrian-rest dependency #4

--- setup.py
+++ setup.py
@@ -2,16 +2,16 @@ from setuptools import setup
 
 setup(
     name='datachile',
-    version='0.0.10',
+    version='0.0.11',
     description='Python client for manage DataChile API',
     url='http://github.com/datachile/datachile-python-client',
-    download_url='https://github.com/datachile/datachile-python-client/archive/0.0.10.tar.gz',
+    download_url='https://github.com/datachile/datachile-python-client/archive/0.0.11.tar.gz',
     author='Carlos Navarrete - Datawheel LLC',
     author_email='cnavarreteliz@gmail.com',
     license='MIT',
     packages=['datachile'],
     install_requires=[
-        'mondrian_rest',
+        'mondrian-rest',
         'numpy',
         'requests'
     ],
